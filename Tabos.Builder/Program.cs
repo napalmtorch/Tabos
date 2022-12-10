5a30db52f09789cd441e302dba96b6391feb599a
napalmtorch <smodgehero@gmail.com>
2022-12-10 17:00:38 -0500

    First commit

---
+++ Tabos.Builder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tabos.Builder;
+
+public static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, world!");
+        Console.Read();
+    }
+}
